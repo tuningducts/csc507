25e95e7c74903370eea69703386a9bb46f3a93e1
tuningducts <tuning-ducts.0j@icloud.com>
2025-06-01 22:04:59 -0500

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+start= time.time()
+file = "file2.txt"
+
+with open(file, "a") as file:
+    for _ in range(1000000):
+        num = random.randint(0, 32767)
+        file.write(f"{num}\n")
+
+end = time.time()
+elapsed_time = end - start
+print(f"program took {elapsed_time:.3f} seconds")
